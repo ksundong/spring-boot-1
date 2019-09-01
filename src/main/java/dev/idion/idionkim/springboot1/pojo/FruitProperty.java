65dfcec957f01e4abfdfb9571c1fe33833e5ff9d
idionKim <ksundong@gmail.com>
2019-09-01 20:32:06 +0900

    Add ConfigurationProperties Test
    the fruit property binding into list in FruitProperty class
    ConfigurationProperties Annotation can mapping not only Collection but also POJO.

---
+++ src/main/java/dev/idion/idionkim/springboot1/pojo/FruitProperty.java
@@ -0,0 +1,15 @@
+package dev.idion.idionkim.springboot1.pojo;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Component
+@ConfigurationProperties("fruit")
+public class FruitProperty {
+	private List<Map> list;
+}
