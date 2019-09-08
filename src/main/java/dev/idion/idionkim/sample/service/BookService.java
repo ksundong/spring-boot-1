d96bac7a45401248f376220f284dff37de5ef827
idionKim <ksundong@gmail.com>
2019-09-08 15:42:20 +0900

    Add Sample WebMVCTest
    The @WebMvcTest annotation is suitable for testing Controller.
    This can test web request and response.
    In addition, it automatically tests Security or Filter and can add/delete manually.
    To use @WebMvcTest, must specify specific Controller name.
    Perform a lightweight MVC test by loading only the beans related to the controller.
    
    The service implementation is being replaced by @MockBean.
    
    The MockMvc can test URL HTTP status, return value.

---
+++ src/main/java/dev/idion/idionkim/sample/service/BookService.java
@@ -0,0 +1,9 @@
+package dev.idion.idionkim.sample.service;
+
+import dev.idion.idionkim.sample.domain.Book;
+
+import java.util.List;
+
+public interface BookService {
+	List<Book> getBookList();
+}
