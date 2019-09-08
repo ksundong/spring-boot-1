f3851cf9fbc3984fc9a25accf9a36f8adf5cb161
idionKim <ksundong@gmail.com>
2019-09-08 15:23:54 +0900

    Update SpringBoot1ApplicationTests for test to work.

--- src/test/java/dev/idion/idionkim/springboot1/SpringBoot1ApplicationTests.java
+++ src/test/java/dev/idion/idionkim/springboot1/SpringBoot1ApplicationTests.java
@@ -4,27 +4,22 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(value = "value=test", properties = {"property.value=propertyTest"}
-                , classes = {SpringBoot1ApplicationTests.class}
-                , webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(value = "value=test")
 public class SpringBoot1ApplicationTests {
 
 	@Value("${value}")
 	private String value;
 
-	@Value("${property.value}")
-	private String propertyValue;
-
 	@Test
 	public void contextLoads() {
 		assertThat(value, is("test"));
-		assertThat(propertyValue, is("propertyTest"));
 	}
 
 }
