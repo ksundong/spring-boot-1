74c00108b57b95a47af48cbc7861b26e334769bf
IdionKim <ksundong@gmail.com>
2019-09-18 00:23:05 +0900

    Add session checking logic. If User object exists don't create a User object

--- src/main/java/dev/idion/idionkim/board/resolver/UserArgumentResolver.java
+++ src/main/java/dev/idion/idionkim/board/resolver/UserArgumentResolver.java
@@ -3,11 +3,17 @@ package dev.idion.idionkim.board.resolver;
 import dev.idion.idionkim.board.annotaion.SocialUser;
 import dev.idion.idionkim.board.domain.User;
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import javax.servlet.http.HttpSession;
+
+@Component
 public class UserArgumentResolver implements HandlerMethodArgumentResolver {
 
 	@Override
@@ -20,7 +26,9 @@ public class UserArgumentResolver implements HandlerMethodArgumentResolver {
 			, ModelAndViewContainer mavContainer
 			, NativeWebRequest webRequest
 			, WebDataBinderFactory binderFactory) throws Exception {
-		return null;
+		HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
+		User user = (User) session.getAttribute("user");
+		return getUser(user, session);
 	}
 
 }
