500f4b3c3baeb6c577db891b309644b9a70f059b
IdionKim <ksundong@gmail.com>
2019-09-18 00:14:42 +0900

    Add specific annotation checking logic
    MethodParameter gets parameters. And supportsParameter only return parameter having SocialUser annotation and User type class.

--- src/main/java/dev/idion/idionkim/board/resolver/UserArgumentResolver.java
+++ src/main/java/dev/idion/idionkim/board/resolver/UserArgumentResolver.java
@@ -1,5 +1,7 @@
 package dev.idion.idionkim.board.resolver;
 
+import dev.idion.idionkim.board.annotaion.SocialUser;
+import dev.idion.idionkim.board.domain.User;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -10,7 +12,8 @@ public class UserArgumentResolver implements HandlerMethodArgumentResolver {
 
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
-		return false;
+		return parameter.getParameterAnnotation(SocialUser.class) != null &&
+				parameter.getParameterType().equals(User.class);
 	}
 
 	public Object resolveArgument(MethodParameter parameter
