a88b9e8f9fd78a9214a455546b82bf484502edec
IdionKim <ksundong@gmail.com>
2019-09-18 00:42:39 +0900

    Add getUser Method

--- src/main/java/dev/idion/idionkim/board/resolver/UserArgumentResolver.java
+++ src/main/java/dev/idion/idionkim/board/resolver/UserArgumentResolver.java
@@ -2,7 +2,13 @@ package dev.idion.idionkim.board.resolver;
 
 import dev.idion.idionkim.board.annotaion.SocialUser;
 import dev.idion.idionkim.board.domain.User;
+import dev.idion.idionkim.board.domain.enums.SocialType;
 import org.springframework.core.MethodParameter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -12,6 +18,13 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static dev.idion.idionkim.board.domain.enums.SocialType.FACEBOOK;
+import static dev.idion.idionkim.board.domain.enums.SocialType.GOOGLE;
+import static dev.idion.idionkim.board.domain.enums.SocialType.KAKAO;
 
 @Component
 public class UserArgumentResolver implements HandlerMethodArgumentResolver {
@@ -29,6 +42,61 @@ public class UserArgumentResolver implements HandlerMethodArgumentResolver {
 		HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
 		User user = (User) session.getAttribute("user");
 		return getUser(user, session);
+	}
+
+	private User getUser(User user, HttpSession session) {
+		if (user == null) {
+			try {
+				OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
+				Map<String, String> map = (HashMap<String, String>) authentication.getUserAuthentication().getDetails();
+				User convertUser = convertUser(String.valueOf(authentication.getAuthorities().toArray()[0]), map);
+
+				user = userRepository.findByEmail(convertUser.getEmail());
+				if (user == null) { user = userRepository.save(convertUser); }
+
+				setRoleIfNotSame(user, authentication, map);
+				session.setAttribute("user", user);
+			} catch (ClassCastException e) {
+				return user;
+			}
+		}
+		return user;
+	}
+
+	private User convertUser(String authority, Map<String, String> map) {
+		if (FACEBOOK.isEquals(authority)) return getModernUser(FACEBOOK, map);
+		else if (GOOGLE.isEquals(authority)) return getModernUser(GOOGLE, map);
+		else if (KAKAO.isEquals(authority)) return getKaKaoUser(map);
+		return null;
+	}
+
+	private User getModernUser(SocialType socialType, Map<String, String> map) {
+		return User.builder()
+				.name(map.get("name"))
+				.email(map.get("email"))
+				.principal(map.get("id"))
+				.socialType(socialType)
+				.createdDate(LocalDateTime.now())
+				.build();
+	}
+
+	private User getKaKaoUser(Map<String, String> map) {
+		HashMap<String, String> propertyMap = (HashMap<String, String>)(Object) map.get("properties");
+		return User.builder()
+				.name(propertyMap.get("nickname"))
+				.email(map.get("kaccount_email"))
+				.principal(String.valueOf(map.get("id")))
+				.socialType(KAKAO)
+				.createdDate(LocalDateTime.now())
+				.build();
+	}
+
+	private void setRoleIfNotSame(User user, OAuth2Authentication authentication, Map<String, String> map) {
+		if (!authentication.getAuthorities().contains(new SimpleGrantedAuthority(user.getSocialType().getRoleType()))) {
+			SecurityContextHolder.getContext().setAuthentication(new
+					UsernamePasswordAuthenticationToken(map, "N/A",
+					AuthorityUtils.createAuthorityList(user.getSocialType().getRoleType())));
+		}
 	}
 
 }
