9deb72a7dcef9c0f92c95060c68d7f1e149c2da8
IdionKim <ksundong@gmail.com>
2019-09-21 15:21:46 +0900

    Add Kakao OAuth2 Login Configuration

--- src/main/java/dev/idion/idionkim/board/config/SecurityConfig.java
+++ src/main/java/dev/idion/idionkim/board/config/SecurityConfig.java
@@ -1,12 +1,24 @@
 package dev.idion.idionkim.board.config;
 
+import dev.idion.idionkim.board.oauth2.CustomOAuth2Provider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static dev.idion.idionkim.board.domain.enums.SocialType.FACEBOOK;
 import static dev.idion.idionkim.board.domain.enums.SocialType.GOOGLE;
@@ -47,6 +59,44 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 				.addFilterBefore(filter, CsrfFilter.class)
 				.csrf().disable();
+	}
+
+	@Bean
+	public ClientRegistrationRepository clientRegistrationRepository(OAuth2ClientProperties oAuth2ClientProperties,
+	                                                                 @Value("${custom.oauth2.kakao.client-id}") String kakaoClientId) {
+		List<ClientRegistration> registrations = oAuth2ClientProperties.getRegistration().keySet().stream()
+				.map(client -> getRegistration(oAuth2ClientProperties, client))
+				.filter(Objects::nonNull)
+				.collect(Collectors.toList());
+
+		registrations.add(CustomOAuth2Provider.KAKAO.getBuilder("kakao")
+				.clientId(kakaoClientId)
+				.clientSecret("test") // not necessary value but not be null value
+				.jwkSetUri("test")// not necessary value but not be null value
+				.build());
+
+		return new InMemoryClientRegistrationRepository(registrations);
+	}
+
+	private ClientRegistration getRegistration(OAuth2ClientProperties clientProperties, String client) {
+		if ("google".equals(client)) {
+			OAuth2ClientProperties.Registration registration = clientProperties.getRegistration().get("google");
+			return CommonOAuth2Provider.GOOGLE.getBuilder(client)
+					.clientId(registration.getClientId())
+					.clientSecret(registration.getClientSecret())
+					.scope("email", "profile")
+					.build();
+		}
+		if ("facebook".equals(client)) {
+			OAuth2ClientProperties.Registration registration = clientProperties.getRegistration().get("facebook");
+			return CommonOAuth2Provider.FACEBOOK.getBuilder(client)
+					.clientId(registration.getClientId())
+					.clientSecret(registration.getClientSecret())
+					.userInfoUri("https://graph.facebook.com/me?fields=id,name,email,link")
+					.scope("email")
+					.build();
+		}
+		return null;
 	}
 
 }
