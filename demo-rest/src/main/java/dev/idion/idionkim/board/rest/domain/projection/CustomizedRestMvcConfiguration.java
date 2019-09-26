827c5411560c7d9ac6ff8688a2dbd3d781385621
IdionKim <ksundong@gmail.com>
2019-09-26 17:07:48 +0900

    Add Manual Projection

---
+++ demo-rest/src/main/java/dev/idion/idionkim/board/rest/domain/projection/CustomizedRestMvcConfiguration.java
@@ -0,0 +1,14 @@
+package dev.idion.idionkim.board.rest.domain.projection;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class CustomizedRestMvcConfiguration extends RepositoryRestConfigurerAdapter {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.getProjectionConfiguration().addProjection(UserOnlyContainName.class);
+	}
+}
