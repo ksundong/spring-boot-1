5c1e48364690336a3ce9ace0bd0b5bda375ca996
IdionKim <ksundong@gmail.com>
2019-09-26 17:12:44 +0900

    Add method that creating a temporary admin user

--- demo-rest/src/main/java/dev/idion/idionkim/board/rest/DataRestApplication.java
+++ demo-rest/src/main/java/dev/idion/idionkim/board/rest/DataRestApplication.java
@@ -2,13 +2,20 @@ package dev.idion.idionkim.board.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class DataRestApplication {
@@ -21,6 +28,18 @@ public class DataRestApplication {
 	@EnableGlobalMethodSecurity(prePostEnabled = true)
 	@EnableWebSecurity
 	static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+		@Bean
+		InMemoryUserDetailsManager userDetailsManager() {
+			User.UserBuilder commonUser = User.withUsername("commonUser").password("{noop}common").roles("USER");
+			User.UserBuilder adminUser = User.withUsername("adminUser").password("{noop}test").roles("USER", "ADMIN");
+
+			List<UserDetails> userDetailsList = new ArrayList<>();
+			userDetailsList.add(commonUser.build());
+			userDetailsList.add(adminUser.build());
+
+			return new InMemoryUserDetailsManager(userDetailsList);
+		}
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
