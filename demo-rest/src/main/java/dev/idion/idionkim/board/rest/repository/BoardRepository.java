d3470bd94549447eb0b7d17747791a3de55b5613
IdionKim <ksundong@gmail.com>
2019-09-26 17:38:26 +0900

    Test Authorization

--- demo-rest/src/main/java/dev/idion/idionkim/board/rest/repository/BoardRepository.java
+++ demo-rest/src/main/java/dev/idion/idionkim/board/rest/repository/BoardRepository.java
@@ -1,11 +1,12 @@
 package dev.idion.idionkim.board.rest.repository;
 
 import dev.idion.idionkim.board.rest.domain.Board;
+import dev.idion.idionkim.board.rest.domain.projection.BoardOnlyContainTitle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
-@RepositoryRestResource
+@RepositoryRestResource(excerptProjection = BoardOnlyContainTitle.class)
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
 	@Override
