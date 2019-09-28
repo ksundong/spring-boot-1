b6056ca6a06bb3721a641f974b9f4b58ae0531c1
idionKim <ksundong@gmail.com>
2019-09-28 09:52:56 +0900

    Add findByTitle feature
    You can access this URI
    e.g.)http://localhost:8081/api/boards/search/findByTitle?title='Search String'

--- demo-rest/src/main/java/dev/idion/idionkim/board/rest/repository/BoardRepository.java
+++ demo-rest/src/main/java/dev/idion/idionkim/board/rest/repository/BoardRepository.java
@@ -3,8 +3,12 @@ package dev.idion.idionkim.board.rest.repository;
 import dev.idion.idionkim.board.rest.domain.Board;
 import dev.idion.idionkim.board.rest.domain.projection.BoardOnlyContainTitle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.List;
 
 @RepositoryRestResource(excerptProjection = BoardOnlyContainTitle.class)
 public interface BoardRepository extends JpaRepository<Board, Long> {
@@ -12,4 +16,7 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 	@Override
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	<S extends Board> S save(S entity);
+
+	@RestResource
+	List<Board> findByTitle(@Param("title") String title);
 }
