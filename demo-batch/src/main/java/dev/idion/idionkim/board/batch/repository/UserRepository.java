e69d6271c1a920b73b22491af0ee06a0e6e6c5a0
idionKim <ksundong@gmail.com>
2019-09-29 15:17:19 +0900

    Add Application, UserRepository

---
+++ demo-batch/src/main/java/dev/idion/idionkim/board/batch/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package dev.idion.idionkim.board.batch.repository;
+
+import dev.idion.idionkim.board.batch.domain.User;
+import dev.idion.idionkim.board.batch.domain.enums.UserStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	List<User> findByUpdatedDateBeforeAndStatusEquals(LocalDateTime localDateTime, UserStatus status);
+}
