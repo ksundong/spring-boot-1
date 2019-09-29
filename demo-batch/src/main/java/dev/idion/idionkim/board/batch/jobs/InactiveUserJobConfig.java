ced9800f2632e3a77cc9d42bf8277e00787bfb02
idionKim <ksundong@gmail.com>
2019-09-29 17:19:43 +0900

    Add Job Parameter for Job execution time equality

--- demo-batch/src/main/java/dev/idion/idionkim/board/batch/jobs/InactiveUserJobConfig.java
+++ demo-batch/src/main/java/dev/idion/idionkim/board/batch/jobs/InactiveUserJobConfig.java
@@ -2,6 +2,7 @@ package dev.idion.idionkim.board.batch.jobs;
 
 import dev.idion.idionkim.board.batch.domain.User;
 import dev.idion.idionkim.board.batch.domain.enums.UserStatus;
+import dev.idion.idionkim.board.batch.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -10,14 +11,16 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.database.JpaItemWriter;
-import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManagerFactory;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
 
 @AllArgsConstructor
 @Configuration
@@ -32,10 +35,10 @@ public class InactiveUserJobConfig {
 	}
 
 	@Bean
-	public Step inactiveJobStep(StepBuilderFactory stepBuilderFactory, JpaPagingItemReader<User> inactiveUserJpaReader) {
+	public Step inactiveJobStep(StepBuilderFactory stepBuilderFactory, ListItemReader<User> inactiveUserReader) {
 		return stepBuilderFactory.get("inactiveUserStep")
 				.<User, User>chunk(CHUNK_SIZE)
-				.reader(inactiveUserJpaReader)
+				.reader(inactiveUserReader)
 				.processor(inactiveUserProcessor())
 				.writer(inactiveUserWriter())
 				.build();
@@ -43,24 +46,10 @@ public class InactiveUserJobConfig {
 
 	@Bean(destroyMethod = "")
 	@StepScope
-	public JpaPagingItemReader inactiveUserJpaReader() {
-		JpaPagingItemReader jpaPagingItemReader = new JpaPagingItemReader() {
-			@Override
-			public int getPage() {
-				return 0;
-			}
-		};
-		jpaPagingItemReader.setQueryString("select u from User as u where u.updatedDate < :updatedDate and u.status = :status");
-
-		Map<String, Object> map = new HashMap<>();
-		LocalDateTime now = LocalDateTime.now();
-		map.put("updatedDate", now.minusYears(1));
-		map.put("status", UserStatus.ACTIVE);
-
-		jpaPagingItemReader.setParameterValues(map);
-		jpaPagingItemReader.setEntityManagerFactory(entityManagerFactory);
-		jpaPagingItemReader.setPageSize(CHUNK_SIZE);
-		return jpaPagingItemReader;
+	public ListItemReader<User> inactiveUserReader(@Value("#{jobParameters[nowDate]}") Date nowDate, UserRepository userRepository) {
+		LocalDateTime now = LocalDateTime.ofInstant(nowDate.toInstant(), ZoneId.systemDefault());
+		List<User> inactiveUsers = userRepository.findByUpdatedDateBeforeAndStatusEquals(now.minusYears(1), UserStatus.ACTIVE);
+		return new ListItemReader<>(inactiveUsers);
 	}
 
 	private ItemProcessor<User, User> inactiveUserProcessor() {
